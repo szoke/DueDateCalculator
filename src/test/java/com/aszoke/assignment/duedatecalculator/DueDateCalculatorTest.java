3b4912e9058af32871f1ace5e71444e816eb5902
Andras Szoke <szokeata@freemail.hu>
2019-08-08 16:36:29 +0200

    Add testCalculateDueDateShouldReturnCreationDateWhenTurnAroundTimeIsZero

--- src/test/java/com/aszoke/assignment/duedatecalculator/DueDateCalculatorTest.java
+++ src/test/java/com/aszoke/assignment/duedatecalculator/DueDateCalculatorTest.java
@@ -6,11 +6,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.time.format.DateTimeParseException;
+import java.time.LocalDateTime;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class DueDateCalculatorTest {
+
+    private static final String TEST_CREATION_DATE_TIME_AS_STRING = "2019-08-08T16:30:55";
+    private static final LocalDateTime TEST_CREATION_DATE_TIME_AS_LOCAL_DATE_TIME =
+            LocalDateTime.parse(TEST_CREATION_DATE_TIME_AS_STRING);
+    private static final int TEST_TURNAROUND_TIME_ZERO = 0;
 
     @Mock
     private LocalDateTimeValidator localDateTimeValidator;
@@ -45,6 +51,26 @@ public class DueDateCalculatorTest {
 
         underTest.calculateDueDate(testCreationDateTime, 1);
 
+        verify(localDateTimeValidator).validate(testCreationDateTime);
+        verify(turnaroundTimeValidator).validate(testTurnaroundTime);
+    }
+
+    @Test
+    public void testCalculateDueDateShouldReturnCreationDateWhenTurnAroundTimeIsZero() {
+        givenValidInputParameters();
+
+        LocalDateTime actual = underTest.calculateDueDate(TEST_CREATION_DATE_TIME_AS_STRING, TEST_TURNAROUND_TIME_ZERO);
+
+        verifyInputValidated(TEST_CREATION_DATE_TIME_AS_STRING, TEST_TURNAROUND_TIME_ZERO);
+        assertEquals(TEST_CREATION_DATE_TIME_AS_LOCAL_DATE_TIME, actual);
+    }
+
+    private void givenValidInputParameters() {
+        doNothing().when(localDateTimeValidator).validate(anyString());
+        doNothing().when(turnaroundTimeValidator).validate(anyInt());
+    }
+
+    private void verifyInputValidated(String testCreationDateTime, int testTurnaroundTime) {
         verify(localDateTimeValidator).validate(testCreationDateTime);
         verify(turnaroundTimeValidator).validate(testTurnaroundTime);
     }
