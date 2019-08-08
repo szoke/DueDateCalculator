90dbab7fb3bba56360eb1615a29e32f89203513f
Andras Szoke <szokeata@freemail.hu>
2019-08-08 15:28:36 +0200

    Make unit test 'throw on null creationDateTime' pass

--- src/main/java/com/aszoke/assignment/duedatecalculator/DueDateCalculator.java
+++ src/main/java/com/aszoke/assignment/duedatecalculator/DueDateCalculator.java
@@ -5,6 +5,14 @@ import java.time.LocalDateTime;
 class DueDateCalculator {
 
     LocalDateTime calculateDueDate(String creationDateTime, int turnaroundTimeInHours) {
+        assertCreationDateTimeIsNotNull(creationDateTime);
+
         return null;
+    }
+
+    private void assertCreationDateTimeIsNotNull(String creationDateTime) {
+        if (creationDateTime == null) {
+            throw new IllegalArgumentException("creationDateTime must not be null!");
+        }
     }
 }
