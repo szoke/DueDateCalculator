12f1179f8c6b2aaa1989bf7019565744a98dbd74
Andras Szoke <szokeata@freemail.hu>
2019-08-08 17:56:04 +0200

    Add testCalculateDueDateShouldReturnWorkEndWhenTurnaroundTimeIsDurationOfWorkday

--- src/test/java/com/aszoke/assignment/duedatecalculator/DueDateCalculatorTest.java
+++ src/test/java/com/aszoke/assignment/duedatecalculator/DueDateCalculatorTest.java
@@ -12,15 +12,21 @@ public class DueDateCalculatorTest {
 
     private static final int TURNAROUND_TIME_ZERO = 0;
     private static final int TURNAROUND_TIME_ONE_HOUR = 1;
+    private static final int TURNAROUND_TIME_ENTIRE_WORKDAY = 8;
 
     private static final LocalTime WORK_START = LocalTime.of(9, 0, 0);
+    private static final LocalTime WORK_END = LocalTime.of(17, 0, 0);
+
     private static final LocalDate MONDAY = LocalDate.of(2019, 8, 5);
     private static final LocalDateTime MONDAY_WORK_START = LocalDateTime.of(MONDAY, WORK_START);
+    private static final LocalDateTime MONDAY_WORK_END = LocalDateTime.of(MONDAY, WORK_END);
 
     private static final LocalDateTime EXPECTED_DUE_DATE_MONDAY_WORK_START_ZERO_TURNAROUND =
             MONDAY_WORK_START;
     private static final LocalDateTime EXPECTED_DUE_DATE_MONDAY_WORK_START_ONE_HOUR_TURNAROUND =
             MONDAY_WORK_START.plusHours(1L);
+    private static final LocalDateTime EXPECTED_DUE_DATE_MONDAY_WORK_START_ENTIRE_WORKDAY_TURNAROUND =
+            MONDAY_WORK_END;
 
     private final DueDateCalculator underTest = new DueDateCalculator();
 
@@ -36,5 +42,12 @@ public class DueDateCalculatorTest {
         LocalDateTime actual = underTest.calculateDueDate(MONDAY_WORK_START, TURNAROUND_TIME_ONE_HOUR);
 
         assertEquals(EXPECTED_DUE_DATE_MONDAY_WORK_START_ONE_HOUR_TURNAROUND, actual);
+    }
+
+    @Test
+    public void testCalculateDueDateShouldReturnWorkEndWhenTurnaroundTimeIsDurationOfWorkday() {
+        LocalDateTime actual = underTest.calculateDueDate(MONDAY_WORK_START, TURNAROUND_TIME_ENTIRE_WORKDAY);
+
+        assertEquals(EXPECTED_DUE_DATE_MONDAY_WORK_START_ENTIRE_WORKDAY_TURNAROUND, actual);
     }
 }
